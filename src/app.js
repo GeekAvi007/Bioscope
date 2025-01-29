de31c506331db0a635a10c815e05355ef0afdbe7
Avishek <avishekmukherje007@gmail.com>
2025-01-30 01:02:13 +0530

    Health Check Routes and API testing

--- src/app.js
+++ src/app.js
@@ -9,9 +9,16 @@ app.use(
         credentials: true
     })
 )
+
 // common middleware
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded( {extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+
+// bring in routes
+import healtcheckRouter from "./routes/healthcheck.routes.js"
+
+// routes
+app.use("/api/v1/healthCheck", healtcheckRouter)
 
 export { app }
